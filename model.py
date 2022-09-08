c83ccdfd2da595d387ddf5d33958faafa5b862d6
Med <mouhammedbeji@gmail.com>
2022-09-08 13:27:56 +0200

    Fix issue related to oversampling
    Fix issue related to oversampling when applying oversampling technique before split and test datasets.

--- model.py
+++ model.py
@@ -1,7 +1,3 @@
-# Implémenter le meme process mais en utilisant les pipelines pour faciliter l'inférence
-import colored_traceback
-colored_traceback.add_hook()
-
 import numpy as np 
 import pandas as pd 
 
@@ -10,6 +6,7 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.utils import resample
+
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import accuracy_score, roc_auc_score,classification_report
@@ -17,84 +14,88 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.base import BaseEstimator, TransformerMixin #gives fit_transform method for free
 
 
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 # save the model to disk
 import pickle 
 
 
-def preprocess_dataset(data: pd.DataFrame, selected_features: list, target: str):
+def preprocess_dataset(data: pd.DataFrame):
     """
         applying the preprocessing of datasets following the following steps: 
         - normalizing data
-        - handling class imbalance
         - removing outliers
         - inputing nulls categorical values
     """
 
-    # normalizing rain today and tommorow columns 
-    data['RainToday'].replace({'No': 0, 'Yes': 1},inplace = True)
-    data['RainTomorrow'].replace({'No': 0, 'Yes': 1},inplace = True)
+    data_df = data.copy()
 
+    # normalizing rain today and tommorow columns 
+    data_df['RainToday'].replace({'No': 0, 'Yes': 1},inplace = True)
+    data_df['RainTomorrow'].replace({'No': 0, 'Yes': 1},inplace = True)
+
+    # Imputer les variables catégorielle avec la méthode mode()
+    subsets = []
+
+    for is_raining in [0, 1]:
+        temp_df = data_df[data_df['RainTomorrow'] == is_raining]
+
+        temp_df['Date'] = temp_df['Date'].fillna(temp_df['Date'].mode()[0])
+        temp_df['Location'] = temp_df['Location'].fillna(temp_df['Location'].mode()[0])
+        temp_df['WindGustDir'] = temp_df['WindGustDir'].fillna(temp_df['WindGustDir'].mode()[0])
+        temp_df['WindDir9am'] = temp_df['WindDir9am'].fillna(temp_df['WindDir9am'].mode()[0])
+        temp_df['WindDir3pm'] = temp_df['WindDir3pm'].fillna(temp_df['WindDir3pm'].mode()[0])
+
+        # Removing outliers 
+        Q1 = temp_df.quantile(0.25)
+        Q3 = temp_df.quantile(0.75)
+        IQR = Q3 - Q1
+        temp_df = temp_df[~((temp_df < (Q1 - 1.5 * IQR)) | (temp_df > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+        # append the values to subsets  
+        subsets.append(temp_df)
+
+    return pd.concat(subsets)
+
+
+def apply_oversampling(data: pd.DataFrame): 
+    """ Handling class imbalance using oversampling method """
     # handling class imbalance
     no = data[data.RainTomorrow == 0]
     yes = data[data.RainTomorrow == 1]
     yes_oversampled = resample(yes, replace=True, n_samples=len(no), random_state=123)
     oversampled = pd.concat([no, yes_oversampled])
-
-    # Imputer les variables catégorielle avec la méthode mode()
-    oversampled['Date'] = oversampled['Date'].fillna(oversampled['Date'].mode()[0])
-    oversampled['Location'] = oversampled['Location'].fillna(oversampled['Location'].mode()[0])
-    oversampled['WindGustDir'] = oversampled['WindGustDir'].fillna(oversampled['WindGustDir'].mode()[0])
-    oversampled['WindDir9am'] = oversampled['WindDir9am'].fillna(oversampled['WindDir9am'].mode()[0])
-    oversampled['WindDir3pm'] = oversampled['WindDir3pm'].fillna(oversampled['WindDir3pm'].mode()[0])
-
-    # Removing outliers 
-    Q1 = oversampled.quantile(0.25)
-    Q3 = oversampled.quantile(0.75)
-    IQR = Q3 - Q1
-
-    print(oversampled.shape)
-
-    data_preprocessed = oversampled[~((oversampled < (Q1 - 1.5 * IQR)) |(oversampled > (Q3 + 1.5 * IQR))).any(axis=1)]
+    return oversampled
     
-    print(data_preprocessed.shape)
-    
-    # separating X & y
-    X = data_preprocessed[FEATURES]
-    y = data_preprocessed[target]
-
-    print(' The shape of dataset after preprocessing ', X.shape)
-
-    return X, y
 
 data = pd.read_csv("data/weatherAus.csv", header=0, squeeze=True)
+
 FEATURES = ['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine', 'WindGustDir', 
                        'WindGustSpeed', 'WindDir9am', 'WindDir3pm', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 
                        'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am', 'Temp3pm', 
                        'RainToday']
-X, y = preprocess_dataset(data=data, selected_features=FEATURES, target='RainTomorrow')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=13)
+data = preprocess_dataset(data=data)
 
-numeric_features =  X.select_dtypes(include= np.number).columns
+target = 'RainTomorrow'
+
+X_train, X_test, y_train, y_test = train_test_split(data, data[target], test_size=0.2, random_state=13)
+
+X_train = apply_oversampling(data= X_train)
+
+y_train = X_train[target]
+X_train = X_train[FEATURES]
+X_test = X_test[FEATURES]
+
+
+numeric_features =  X_train.select_dtypes(include= np.number).columns
 numeric_transformer = Pipeline(steps=[('imputer', IterativeImputer()),
     ('scaler', MinMaxScaler())])
 
 
-
-class MyLabelBinarizer(BaseEstimator, TransformerMixin):
-
-    def __init__(self):
-        self.encoder = LabelEncoder()
-    def fit(self, x, y=0):
-        self.encoder.fit(x)
-        return self
-    def transform(self, x, y=0):
-        return self.encoder.transform(x)
-
-    def fit_transform(self, x, y=0):
-        return self.encoder.fit(x).transform(x)
-
-categorical_features = X.select_dtypes(include=['object']).columns
+categorical_features = X_train.select_dtypes(include=['object']).columns
 categorical_transformer = OneHotEncoder()
 
 preprocessor = ColumnTransformer(
@@ -113,11 +114,9 @@ params_rf = {'max_depth': 16,
 clf = Pipeline(steps=[('preprocessor', preprocessor),
                       ('classifier', RandomForestClassifier(**params_rf))])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
-                                                    random_state=0)
 
+print('Training the models.. with X shape = ', X_train.shape, 'y shape', y_train.shape)
 clf.fit(X_train, y_train)
-
 y_pred = clf.predict(X_test)
 
 accuracy = accuracy_score(y_test, y_pred)
@@ -128,5 +127,41 @@ print(roc_auc)
 print(classification_report(y_test,y_pred,digits=5))
 
 
-filename = 'model_13_08.sav'
+filename = 'model_09_08.sav'
 pickle.dump(clf, open('models/' + filename, 'wb'))
+
+
+
+""""
+
+    Dataset split Train 70% / Test 30%
+    Scores généré sont : 
+    accuracy => 0.8973683394811072
+    roc_auc => 0.8651836705828564
+    classification report => 
+                                precision    recall  f1-score   support
+
+                            0.0    0.94214   0.92405   0.93300    24950
+                            1.0    0.75674   0.80632   0.78074     7311
+
+                    accuracy                           0.89737    32261
+                    macro avg      0.84944   0.86518   0.85687    32261
+                    weighted avg   0.90012   0.89737   0.89850    32261
+    ---------------------------------------------------------------------------
+
+    Dataset split Train 80% / Test 20%
+    Scores généré sont : 
+    accuracy => 0.8997535686055703
+    roc_auc => 0.8662161076595627
+    classification report => 
+                                          precision    recall  f1-score   support
+
+                                    0.0    0.94180   0.92764   0.93467     16625
+                                    1.0    0.76559   0.80479   0.78470      4882
+
+                                accuracy                       0.89975     21507
+                            macro avg      0.85369   0.86622   0.85968     21507
+                            weighted avg   0.90180   0.89975   0.90063     21507
+
+
+"""
